d1b811fe434623162c3c9cecbcf2172b130068f7
Kauany7492 <140413644+Kauany7492@users.noreply.github.com>
2026-03-04 15:52:57 -0300

    Refactor database connection with environment variables
    Updated database connection to use environment variables for configuration and added SSL support.

--- database.js
+++ database.js
@@ -1,12 +1,15 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',      // Altere se seu usuário for diferente
-    password: '1234',      // Altere se tiver senha
-    database: 'smart_notebook',
-    waitForConnections: true,
-    connectionLimit: 10
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 4000,
+    ssl: {
+        rejectUnauthorized: false // Permite a conexão segura necessária na nuvem
+    }
 });
 
 module.exports = pool;
